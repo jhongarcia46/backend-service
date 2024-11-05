4b7ce9df3658e009f17b4df78fcbee0ddd4ce56c
jhongarcia46 <jhongarcia46@hotmail.com>
2024-11-05 09:29:41 -0500

    arreglo

--- src/swagger/swagger.ts
+++ src/swagger/swagger.ts
@@ -1,22 +1,23 @@
 import { info } from "console";
-import swaggerJSDoc, { options } from "swagger-jsdoc";
+import swaggerJSDoc, { Options } from "swagger-jsdoc";
 
-const swaggerOptions: options = {
-    definition: {},
-    openapi: "3.0.0",
-    info: {
-        title: "backend Service API",
-        version: "1.0.0",
-        description: "API para catlogo de productos"
+const swaggerOptions: Options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "backend Service API",
+            version: "1.0.0",
+            description: "API para catlogo de productos"
+        },
+        servers: [
+            {
+                url: "http://localhost:3000/"
+            }
+        ]
     },
-    servers: [
-    {
-        url: "http://localhost:3000/"
-    }
-    ]
     apis: [
         "./src/routes/productRouter.ts",
-    ] 
+    ]
 };
 
 const swaggerSpec = swaggerJSDoc(swaggerOptions);
